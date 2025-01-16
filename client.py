40a731008528c6e443c61e8bbffe2e6ea62c7d08
Andrei Burvel <burvelandrei@gmail.com>
2025-01-16 17:52:59 +0300

    V 1.4 добавлен контекстный менеджер для APIClient

--- client.py
+++ client.py
@@ -11,85 +11,86 @@ class APIClient:
     def __init__(self):
         self.domain = f"http://{env('HOST')}:{env('PORT')}"
 
+    async def __aenter__(self):
+        self.session = client.ClientSession()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.session.close()
+
     async def get_books(self):
         url = f"{self.domain}/books/"
         try:
-            async with client.ClientSession() as session:
-                async with session.get(url) as response:
-                    if response.status == 200:
-                        print(response.status)
-                        print(await response.json())
-                    else:
-                        print(f"Ошибка GET запроса - {response.status}")
+            async with self.session.get(url) as response:
+                if response.status == 200:
+                    print(response.status)
+                    print(await response.json())
+                else:
+                    print(f"Ошибка GET запроса - {response.status}")
         except client.ClientError as e:
             print(f"Ошибка сети - {e}")
 
     async def get_book(self, id):
         url = f"{self.domain}/books/{id}/"
         try:
-            async with client.ClientSession() as session:
-                async with session.get(url) as response:
-                    if response.status == 200:
-                        print(response.status)
-                        print(await response.json())
-                    else:
-                        print(f"Ошибка GET запроса - {response.status}")
+            async with self.session.get(url) as response:
+                if response.status == 200:
+                    print(response.status)
+                    print(await response.json())
+                else:
+                    print(f"Ошибка GET запроса - {response.status}")
         except client.ClientError as e:
             print(f"Ошибка сети - {e}")
 
     async def add_book(self, data):
         url = f"{self.domain}/add_book/"
         try:
-            async with client.ClientSession() as session:
-                async with session.post(url, json=data) as response:
-                    if response.status == 201:
-                        print(response.status)
-                        print(await response.json())
-                    else:
-                        print(
-                            f"Ошибка POST запроса - {response.status}, {await response.json()}"
-                        )
+            async with self.session.post(url, json=data) as response:
+                if response.status == 201:
+                    print(response.status)
+                    print(await response.json())
+                else:
+                    print(
+                        f"Ошибка POST запроса - {response.status}, {await response.json()}"
+                    )
         except client.ClientError as e:
             print(f"Ошибка сети - {e}")
 
     async def update_book(self, data):
         url = f"{self.domain}/update_book/"
         try:
-            async with client.ClientSession() as session:
-                async with session.patch(url, json=data) as response:
-                    if response.status == 200:
-                        print(response.status)
-                        print(await response.json())
-                    else:
-                        print(
-                            f"Ошибка PATCH запроса - {response.status}, {await response.json()}"
-                        )
+            async with self.session.patch(url, json=data) as response:
+                if response.status == 200:
+                    print(response.status)
+                    print(await response.json())
+                else:
+                    print(
+                        f"Ошибка PATCH запроса - {response.status}, {await response.json()}"
+                    )
         except client.ClientError as e:
             print(f"Ошибка сети - {e}")
 
     async def delete_book(self, id):
         url = f"{self.domain}/delete_book/{id}/"
         try:
-            async with client.ClientSession() as session:
-                async with session.delete(url) as response:
-                    if response.status == 204:
-                        print(response.status)
-                        print(await response.json())
-                    else:
-                        print(f"Ошибка DELETE запроса - {response.status}")
+            async with self.session.delete(url) as response:
+                if response.status == 204:
+                    print(response.status)
+                    print(await response.json())
+                else:
+                    print(f"Ошибка DELETE запроса - {response.status}")
         except client.ClientError as e:
             print(f"Ошибка сети - {e}")
 
 
+# Создаём таски для проверки
 async def main():
-    api = APIClient()
-    await asyncio.gather(
-        api.get_books(),
-        api.get_book(1),
-        api.add_book({"id": 3, "name": "Война и мир"}),
-        api.update_book({"id": 2, "name": "Собачье"}),
-        api.delete_book(2),
-    )
+    async with APIClient() as api:
+        await asyncio.create_task(api.get_books())
+        await asyncio.create_task(api.get_book(1))
+        await asyncio.create_task(api.add_book({"id": 3, "name": "Война и мир"}))
+        await asyncio.create_task(api.update_book({"id": 2, "name": "Собачье"}))
+        await asyncio.create_task(api.delete_book(2))
 
 
 if __name__ == "__main__":
